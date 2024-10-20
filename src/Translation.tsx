4a5be62f184cad319000e7e009385d98c2a2ea16
Nguyen Tran <nguyen.tran1072e@gmail.com>
2024-10-19 23:55:35 -0400

    Update Translation component

--- src/Translation.tsx
+++ src/Translation.tsx
@@ -1,13 +1,14 @@
 import type { Component, JSXElement } from "solid-js";
-import { createTranslation } from "./createTranslation.ts";
+import { useTranslation, TranslationOptions } from "./useTranslation.ts";
 import type { i18n, TFunction } from "i18next";
 
 interface TranslationProps {
   children: (t: TFunction, i18n: i18n) => JSXElement;
+  options?: TranslationOptions;
 }
 
 export const Translation: Component<TranslationProps> = (props) => {
-  const [t, i18n] = createTranslation();
+  const [t, i18n] = useTranslation(props.options);
 
   return props.children(t, i18n);
 };
