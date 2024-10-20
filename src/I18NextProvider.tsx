1098692a8dcbef99d3aed863d54f50de647689de
Nguyen Tran <nguyen.tran1072e@gmail.com>
2024-10-19 23:55:51 -0400

    Update I18NextProvider

--- src/I18NextProvider.tsx
+++ src/I18NextProvider.tsx
@@ -1,24 +1,21 @@
 import {
-  type Context,
   createContext,
-  createMemo,
   type ParentComponent,
 } from "solid-js";
-import type { i18n, Namespace } from "i18next";
+import { i18n, Namespace } from "i18next";
 
 interface I18nContextObject {
   i18n: i18n;
   ns?: Namespace;
 }
 
-export const I18nContext: Context<I18nContextObject | undefined> =
-  createContext();
+export const I18nContext = createContext<I18nContextObject>();
 
 export const I18NextProvider: ParentComponent<I18nContextObject> = (props) => {
-  const value = createMemo(() => ({ i18n: props.i18n, ns: props.ns }));
+  const value = { i18n: props.i18n, ns: props.ns };
 
   return (
-    <I18nContext.Provider value={value()}>
+    <I18nContext.Provider value={value}>
       {props.children}
     </I18nContext.Provider>
   );
