be736edab41cedca9d66d854a7bc017f6a0fb5c6
Nguyen Tran <nguyen.tran1072e@gmail.com>
2024-10-26 20:55:30 -0400

    First attempt of creating a reactive i18next instance

---
+++ src/reactiveI18next.ts
@@ -0,0 +1,336 @@
+import i18next, {
+  Callback,
+  CloneOptions,
+  InitOptions,
+  KeyPrefix,
+  Module,
+  Namespace,
+  Newable,
+  NewableModule,
+  TFunction,
+  type i18n,
+} from "i18next";
+import { createTrigger } from "@solid-primitives/trigger";
+import { Accessor, createEffect, createSignal, useContext } from "solid-js";
+import { createStore, produce, reconcile } from "solid-js/store";
+import { I18nContext } from "./I18NextProvider";
+
+class ReactiveI18n implements i18n {
+  #i18n: i18n;
+  #i18nTrack: VoidFunction;
+  #i18nDirty: VoidFunction;
+  #i18nStore;
+
+  constructor(i18n: i18n) {
+    const [track, dirty] = createTrigger();
+    this.#i18n = i18n ?? i18next;
+    this.#i18nTrack = track;
+    this.#i18nDirty = dirty;
+
+    const [i18nStore, setI18nStore] = createStore({
+      language: this.#i18n.language,
+      languages: this.#i18n.languages,
+      resolvedLanguage: this.#i18n.resolvedLanguage,
+      isInitialized: this.#i18n.isInitialized,
+      isInitializing: this.#i18n.isInitializing,
+      initializedStoreOnce: this.#i18n.initializedStoreOnce,
+      initializedLanguageOnce: this.#i18n.initializedLanguageOnce,
+    })
+    this.#i18nStore = i18nStore;
+
+    createEffect(() => {
+      this.#i18nTrack();
+
+      setI18nStore(produce(i18nStore => {
+        i18nStore.language = this.#i18n.language;
+        i18nStore.languages = this.#i18n.languages;
+        i18nStore.isInitialized = this.#i18n.isInitialized;
+        i18nStore.isInitializing = this.#i18n.isInitializing;
+        i18nStore.initializedStoreOnce = this.#i18n.initializedStoreOnce;
+        i18nStore.initializedLanguageOnce = this.#i18n.initializedLanguageOnce;
+      }))
+    })
+  }
+
+  // @ts-ignore
+  t(...args: Parameters<i18n["t"]>) {
+    this.#i18nTrack();
+    return this.#i18n.t(...args);
+  }
+
+  init<T>(options: InitOptions<T>, callback?: Callback): Promise<TFunction>;
+  init(callback?: Callback): Promise<TFunction>;
+
+  async init(optionsOrCallback: any, callback?: Callback) {
+    const t = await this.#i18n.init(optionsOrCallback, callback);
+    this.#i18nDirty();
+    return t;
+  }
+
+  loadResources(callback?: (err: any) => void): void {
+    this.#i18nDirty();
+    return this.#i18n.loadResources(callback);
+  }
+
+  use<T extends Module>(module: T | NewableModule<T> | Newable<T>): this {
+    this.#i18nDirty();
+    return this.use(module);
+  }
+
+  get modules() {
+    this.#i18nTrack();
+    return this.#i18n.modules;
+  }
+
+  get services() {
+    this.#i18nTrack();
+    return this.#i18n.services;
+  }
+
+  get store() {
+    this.#i18nTrack();
+    return this.#i18n.store;
+  }
+
+  exists(...args: Parameters<i18n["exists"]>) {
+    this.#i18nTrack();
+    return this.#i18n.exists(...args);
+  }
+
+  getDataByLanguage(
+    lng: string
+  ): { [key: string]: { [key: string]: string } } | undefined {
+    this.#i18nTrack();
+    return this.#i18n.getDataByLanguage(lng);
+  }
+
+  getFixedT<
+    Ns extends Namespace | null = "translation",
+    TKPrefix extends KeyPrefix<ActualNs> = undefined,
+    ActualNs extends Namespace = Ns extends null ? "translation" : Ns
+  >(
+    ...args:
+      | [lng: string | readonly string[], ns?: Ns, keyPrefix?: TKPrefix]
+      | [lng: null, ns: Ns, keyPrefix?: TKPrefix]
+  ): TFunction<ActualNs, TKPrefix> {
+    this.#i18nTrack();
+    return this.#i18n.getFixedT(...args);
+  }
+
+  async changeLanguage(lng?: string, callback?: Callback): Promise<TFunction> {
+    const t = await this.#i18n.changeLanguage(lng, callback);
+    this.#i18nDirty();
+    return t;
+  }
+
+  get language() {
+    return this.#i18nStore.language;
+  }
+
+  get languages() {
+    return this.#i18nStore.languages;
+  }
+
+  get resolvedLanguage() {
+    return this.#i18nStore.resolvedLanguage;
+  }
+
+  hasLoadedNamespace(
+    ns: string | readonly string[],
+    options?: {
+      lng?: string | readonly string[];
+      fallbackLng?: InitOptions["fallbackLng"];
+      precheck?: (
+        i18n: i18n,
+        loadNotPending: (
+          lng: string | readonly string[],
+          ns: string | readonly string[]
+        ) => boolean
+      ) => boolean | undefined;
+    }
+  ): boolean {
+    this.#i18nTrack();
+    return this.#i18n.hasLoadedNamespace(ns, options);
+  }
+
+  async loadNamespaces(
+    ns: string | readonly string[],
+    callback?: Callback
+  ): Promise<void> {
+    await this.#i18n.loadNamespaces(ns, callback);
+    this.#i18nDirty();
+  }
+
+  async loadLanguages(
+    lngs: string | readonly string[],
+    callback?: Callback
+  ): Promise<void> {
+    await this.#i18n.loadLanguages(lngs, callback);
+    this.#i18nDirty();
+  }
+
+  reloadResources(
+    lngs?: string | readonly string[],
+    ns?: string | readonly string[],
+    callback?: () => void
+  ): Promise<void>;
+
+  async reloadResources(
+    lngs: null,
+    ns: string | readonly string[],
+    callback?: () => void
+  ): Promise<void>;
+
+  async reloadResources(
+    lngs: any,
+    ns: any,
+    callback?: () => void
+  ): Promise<void> {
+    await this.#i18n.reloadResources(lngs, ns, callback);
+    this.#i18nDirty();
+  }
+
+  setDefaultNamespace(ns: string): void {
+    this.#i18n.setDefaultNamespace(ns);
+    this.#i18nDirty();
+  }
+
+  dir(lng?: string): "ltr" | "rtl" {
+    this.#i18nTrack();
+    return this.#i18n.dir(lng);
+  }
+
+  format(...args: Parameters<i18n["format"]>) {
+    this.#i18nTrack();
+    return this.#i18n.format(...args);
+  }
+
+  createInstance(options?: InitOptions, callback?: Callback): i18n {
+    return this.#i18n.createInstance(options, callback);
+  }
+
+  createReactiveInstance(
+    options?: InitOptions,
+    callback?: Callback
+  ): ReactiveI18n {
+    const newI18n = this.#i18n.createInstance(options, callback);
+    const reactiveI18n = new ReactiveI18n(newI18n);
+    return reactiveI18n;
+  }
+
+  cloneInstance(options?: CloneOptions, callback?: Callback): i18n {
+    return this.#i18n.cloneInstance(options, callback);
+  }
+
+  cloneReactiveInstance(
+    options?: CloneOptions,
+    callback?: Callback
+  ): ReactiveI18n {
+    const newI18n = this.#i18n.cloneInstance(options, callback);
+    const reactiveI18n = new ReactiveI18n(newI18n);
+    return reactiveI18n;
+  }
+
+  on(
+    event: Parameters<i18n["on"]>[0],
+    listener: Parameters<i18n["on"]>[1]
+  ): void {
+    return this.#i18n.on(event, listener);
+  }
+
+  off(
+    event: Parameters<i18n["off"]>[0],
+    listener: Parameters<i18n["off"]>[1]
+  ): void {
+    return this.#i18n.off(event, listener);
+  }
+
+  getResource(
+    lng: string,
+    ns: string,
+    key: string,
+    options?: Pick<InitOptions, "keySeparator" | "ignoreJSONStructure">
+  ) {
+    this.#i18nTrack();
+    return this.#i18n.getResource(lng, ns, key, options);
+  }
+
+  addResource(
+    lng: string,
+    ns: string,
+    key: string,
+    value: string,
+    options?: { keySeparator?: string; silent?: boolean }
+  ): i18n {
+    const i18n = this.#i18n.addResource(lng, ns, key, value, options);
+    this.#i18nDirty();
+    return i18n;
+  }
+
+  addResources(lng: string, ns: string, resources: any): i18n {
+    const i18n = this.#i18n.addResources(lng, ns, resources);
+    this.#i18nDirty();
+    return i18n;
+  }
+
+  addResourceBundle(
+    lng: string,
+    ns: string,
+    resources: any,
+    deep?: boolean,
+    overwrite?: boolean
+  ): i18n {
+    const i18n = this.#i18n.addResourceBundle(lng, ns, resources, deep, overwrite);
+    this.#i18nDirty();
+    return i18n;
+  }
+
+  hasResourceBundle(lng: string, ns: string): boolean {
+    this.#i18nTrack();
+    return this.#i18n.hasResourceBundle(lng, ns);
+  }
+
+  getResourceBundle(lng: string, ns: string) {
+    this.#i18nTrack();
+    return this.#i18n.getResourceBundle(lng, ns);
+  }
+
+  removeResourceBundle(lng: string, ns: string): i18n {
+    const i18n = this.#i18n.removeResourceBundle(lng, ns);
+    this.#i18nDirty();
+    return i18n;
+  }
+
+  get options() {
+    this.#i18nTrack();
+    return this.#i18n.options;
+  }
+
+  get isInitialized() {
+    return this.#i18nStore.isInitialized;
+  }
+
+  get isInitializing() {
+    return this.#i18nStore.isInitializing;
+  }
+
+  get initializedStoreOnce() {
+    return this.#i18nStore.initializedStoreOnce;
+  }
+
+  get initializedLanguageOnce() {
+    return this.#i18nStore.initializedLanguageOnce;
+  }
+
+  emit(eventName: string, ...args: any[]): void {
+    return this.#i18n.emit(eventName, ...args);
+  }
+}
+
+export const createReactiveI18n = (i18n?: i18n) => {
+  const i18nContext = useContext(I18nContext);
+  const workingI18n = i18n || i18nContext?.i18n || i18next;
+  const reactiveI18n = new ReactiveI18n(workingI18n);
+
+  return reactiveI18n;
+};
---
+++ stories/reactiveI18next.stories.tsx
@@ -0,0 +1,142 @@
+import i18next from "i18next";
+import { I18NextProvider } from "../src";
+import { batch, createEffect } from "solid-js";
+import { Meta } from "@storybook/html";
+import { createReactiveI18n } from "../src/reactiveI18next";
+
+const i18nextInit = {
+  resources: {
+    en: {
+      translation: {
+        "button": "Button in english",
+        "special": {
+          "button": "Special button in english"
+        }
+      },
+      informal: {
+        "button": "Clicky thing in english"
+      }
+    },
+    fr: {
+      translation: {
+        "button": "Button in french",
+        "special": {
+          "button": "Special button in french"
+        }
+      },
+      informal: {
+        "button": "Clicky thing in french"
+      }
+    }
+  },
+  fallbackLng: "en",
+};
+
+i18next.init(i18nextInit);
+
+const meta: Meta = {
+  decorators: [
+    (Story) => {
+      const i18n = i18next.cloneInstance();
+
+      return (
+        <I18NextProvider i18n={i18n}>
+          <Story />
+        </I18NextProvider>
+      ) as Element;
+    }
+  ]
+};
+
+export default meta;
+
+export const LanguagesReactiveOnChangeLanguage = {
+  render: () => {
+    const i18n = createReactiveI18n();
+
+    const handleClick = () => {
+      const lng = i18n.language;
+      const newLng = lng === 'en' ? 'fr' : 'en';
+      i18n.changeLanguage(newLng);
+    }
+
+    createEffect(() => {
+      console.log('i18n.language', i18n.language);
+    })
+
+    createEffect(() => {
+      console.log('i18n.languages', JSON.stringify(i18n.languages));
+    })
+
+    createEffect(() => {
+      console.log('i18n.resolvedLanguage', i18n.resolvedLanguage);
+    })
+
+    createEffect(() => {
+      console.log('i18n.isInitialized', i18n.isInitialized);
+    })
+
+    createEffect(() => {
+      console.log('i18n.isInitializing', i18n.isInitializing);
+    })
+
+    createEffect(() => {
+      console.log('i18n.initializedStoreOnce', i18n.initializedStoreOnce);
+    });
+
+    createEffect(() => {
+      console.log('i18n.initializedLanguageOnce', i18n.initializedLanguageOnce);
+    });
+
+    return (
+      <>
+        <button on:click={handleClick}>Change language</button>
+        <p>i18n.language = {i18n.language}</p>
+        <p>i18n.languages = {JSON.stringify(i18n.languages)}</p>
+        <p>i18n.resolvedLanguage = {i18n.resolvedLanguage}</p>
+        <p>i18n.isInitialized = {JSON.stringify(i18n.isInitialized)}</p>
+        <p>i18n.isInitializing = {JSON.stringify(i18n.isInitializing)}</p>
+      </>
+    ) as Element;
+  }
+}
+
+export const ResourcesReactiveOnAddResources = {
+  render: () => {
+    const i18n = createReactiveI18n();
+
+    const handleAddResource = () => {
+      batch(() => {
+        i18n.addResource("en", "very-informal", "special.button", "Super epic button in English");
+        i18n.addResource("fr", "very-informal", "special.button", "Super epic button in French");
+      });
+    }
+
+    const handleRemoveResource = () => {
+      batch(() => {
+        i18n.removeResourceBundle("en", "very-informal")
+        i18n.removeResourceBundle("fr", "very-informal");
+      });
+    }
+
+    const enBundle = () => JSON.stringify(i18n.getResourceBundle("en", "very-informal"));
+    const hasEnBundle = () => i18n.hasResourceBundle("en", "very-informal");
+
+    const frBundle = () => JSON.stringify(i18n.getResourceBundle("fr", "very-informal"));
+    const hasFrBundle = () => i18n.hasResourceBundle("en", "very-informal");
+
+    createEffect(() => {
+      console.log(enBundle());
+      console.log(frBundle());
+    })
+
+    return (
+      <>
+        <button on:click={handleAddResource}>Add resource</button>
+        <button on:click={handleRemoveResource}>Remove resource</button>
+        <p>{hasEnBundle() ? enBundle() : "No resource bundle for English"}</p>
+        <p>{hasFrBundle() ? frBundle() : "No resource bundle for French"}</p>
+      </>
+    ) as Element;
+  }
+}
