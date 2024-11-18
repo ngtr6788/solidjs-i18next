015d7364c20af87c590c338e8e1da43f664b0acb
Nguyen Tran <nguyen.tran1072e@gmail.com>
2024-11-17 20:43:40 -0500

    Minify

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  // minify: true,
+  minify: true,
   clean: true,
   dts: true,
   format: ["esm", "cjs"],
