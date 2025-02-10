c777135b4c214badb1b8c7ce0657e778f746402d
Nguyen Tran <nguyen.tran1072e@gmail.com>
2025-02-09 22:21:27 -0500

    test(Trans): add some dynamic prop tests

--- tests/Trans.test.tsx
+++ tests/Trans.test.tsx
@@ -1,5 +1,6 @@
 import { render } from "@solidjs/testing-library";
 import i18next from "i18next";
+import { type JSX, type ParentComponent } from "solid-js";
 // import userEvent from "@testing-library/user-event";
 import { describe, expect, test } from "vitest";
 
@@ -20,7 +21,7 @@ const i18nInit = {
         "to-learn-more-click-here":
           "To learn <italics>a whole lot</italics> more, click <CustomLink>here</CustomLink>",
         "bold-italics-underline":
-          "Number <1>one</1>, number <3>three</3>, number <6>six</6>, number <7>seven, <8>eight, </8> <9>nine, <10>ten</10></9></7>",
+          "Number <1>one</1>, number <3>three</3>, number <6>six</6>, number <7>seven, <8>eight</8>, <9>nine, <10>ten</10></9></7>",
         "hello-name-have-number":
           "Hello {{name}}, you have {{numEmails}} unread emails today.",
         "greetings-name-number":
@@ -54,20 +55,132 @@ const i18nInit = {
 i18next.init(i18nInit);
 
 describe("Trans component tests", () => {
-  test("not real test", () => {
-    const Test = () => {
-      return (
-        <Trans
-          i18nKey="click-here-to-subscribe"
-          dynamic={[{ component: "a", href: "" }, { component: "b" }]}
-        />
+  describe("dynamic prop tests", () => {
+    test("basic interpolating with dynamic component array", () => {
+      const Test = () => {
+        return (
+          <Trans
+            i18nKey="click-here-to-subscribe"
+            dynamic={[{ component: "a", href: "" }, { component: "b" }]}
+          />
+        );
+      };
+
+      const screen = render(() => <Test />, {});
+
+      expect(screen.container.innerHTML).toEqual(
+        'Click <a href="">here</a> to <b>subscribe</b>',
       );
-    };
+    });
 
-    const screen = render(() => <Test />, {});
+    test("basic interpolating with dynamic component object", () => {
+      const Test = () => {
+        return (
+          <Trans
+            i18nKey="to-learn-more-click-here"
+            dynamic={{
+              italics: {
+                component: "i",
+              },
+              CustomLink: {
+                component: "a",
+                href: "",
+              },
+            }}
+          />
+        );
+      };
 
-    expect(screen.container.innerHTML).toEqual(
-      'Click <a href="">here</a> to <b>subscribe</b>',
-    );
+      const screen = render(() => <Test />, {});
+      expect(screen.container.innerHTML).toEqual(
+        'To learn <i>a whole lot</i> more, click <a href="">here</a>',
+      );
+    });
+
+    test("no dynamic component object but string built on its own", () => {
+      const Test = () => {
+        return <Trans i18nKey="click-here-to-subscribe-buildable" />;
+      };
+
+      const screen = render(() => <Test />, {});
+      expect(screen.container.innerHTML).toEqual(
+        'Click <strong class="my-class">here</strong> to <i>subscribe</i><br><p>and hit the notification button as well</p>',
+      );
+    });
+
+    test("nested interpolating with tree-like component object", () => {
+      const Link: ParentComponent<JSX.HTMLAttributes<HTMLAnchorElement>> = (
+        props,
+      ) => {
+        return (
+          <a href="" {...props}>
+            {props.children}
+          </a>
+        );
+      };
+
+      const Box: ParentComponent<JSX.HTMLAttributes<HTMLDivElement>> = (
+        props,
+      ) => {
+        return <div class="my-class" {...props} />;
+      };
+
+      const Test = () => {
+        return (
+          <Trans
+            i18nKey="bold-italics-underline"
+            dynamic={{
+              1: {
+                component: "div",
+                style: {
+                  color: "red",
+                },
+              },
+              3: {
+                component: Link,
+                style: {
+                  color: "#4F97A3",
+                  "text-decoration": "none",
+                },
+              },
+              6: {
+                component: "b",
+                style: {
+                  "font-size": "xx-large",
+                },
+              },
+              7: {
+                component: Box,
+                style: {
+                  display: "flex",
+                  "flex-direction": "column-reverse",
+                },
+                children: {
+                  8: {
+                    component: "u",
+                  },
+                  9: {
+                    component: "i",
+                    children: {
+                      10: {
+                        component: Box,
+                        style: {
+                          "background-color": "black",
+                          color: "white",
+                          width: "100px",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            }}
+          />
+        );
+      };
+
+      const screen = render(() => <Test />, {});
+      expect(screen.container.innerHTML).toMatchSnapshot();
+    });
   });
 });
