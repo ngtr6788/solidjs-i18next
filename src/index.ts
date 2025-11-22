58681f2d89e44a99e95234fa2688f0fad18e8d7a
Nguyen Tran <nguyen.tran1072e@gmail.com>
2025-11-22 00:10:38 -0500

    fix(reactivei18next): remove reactivei18next - too complicated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from "./Fragment.tsx";
 export * from "./I18NextProvider.tsx";
-export * from "./reactiveI18next.ts";
 export * from "./Trans.tsx";
 export * from "./Translation.tsx";
 export * from "./TransTypecheck.tsx";
